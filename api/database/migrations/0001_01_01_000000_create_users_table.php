41a47c89bffcced75d0fe74ebe9c0983fd62187f
mariomo16 <mariomo16.cieza@gmail.com>
2026-05-06 22:51:53 +0200

    feat(migrations): add user, zones, reservations, computers, time slots, notifications, and payments tables with necessary fields and relationships

--- api/database/migrations/0001_01_01_000000_create_users_table.php
+++ api/database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,13 +11,17 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id()->autoIncrement();
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('role', ['staff', 'client'])->default('client');
+            $table->decimal('balance', 5, 2)->default(0);
+            $table->string('image')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
---
+++ api/database/migrations/2026_05_06_201906_create_reservations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\User;
+use App\Models\Computer;
+use App\Models\TimeSlot;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->foreignIdFor(User::class)->constrained();
+            $table->foreignIdFor(Computer::class)->constrained();
+            $table->foreignIdFor(TimeSlot::class)->constrained();
+            $table->date('date');
+            $table->enum('status', ['confirmed', 'pending', 'cancelled'])->default('pending');
+            $table->decimal('total_price', 4, 2);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
