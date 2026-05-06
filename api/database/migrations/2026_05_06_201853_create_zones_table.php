fca98cd885e13292bbf1b0bb0b841a50dc673828
mariomo16 <mariomo16.cieza@gmail.com>
2026-05-06 23:30:28 +0200

    feat(migrations): add 'image' field to zones table

--- api/database/migrations/2026_05_06_201853_create_zones_table.php
+++ api/database/migrations/2026_05_06_201853_create_zones_table.php
@@ -14,6 +14,7 @@ return new class extends Migration {
             $table->id()->autoIncrement();
             $table->string('name');
             $table->string('description')->nullable();
+            $table->string('image');
             $table->softDeletes();
         });
     }
