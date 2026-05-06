84eebd67d6db0314d520303f63e1dfa2c8fd6ad1
mariomo16 <mariomo16.cieza@gmail.com>
2026-05-06 20:55:55 +0200

    feat(home): add Home component with routing and basic template

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from "@angular/router";
+import type { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: "",
+		loadComponent: () =>
+			import("./features/home/home").then((module) => module.Home),
+		title: "Inicio",
+	},
+];
