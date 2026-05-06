889b94de5eb59cc615f8e625eef5b9dc1cbef111
mariomo16 <mariomo16.cieza@gmail.com>
2026-05-06 21:18:11 +0200

    feat(web): add empty, error, and loading state components with templates

---
+++ web/src/app/shared/ui/states/error-state/error-state.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+
+@Component({
+	selector: "app-error-state",
+	imports: [],
+	templateUrl: "./error-state.html",
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ErrorState {}
---
+++ web/src/app/shared/ui/states/loading-state/loading-state.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+
+@Component({
+	selector: "app-loading-state",
+	imports: [],
+	templateUrl: "./loading-state.html",
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoadingState {}
