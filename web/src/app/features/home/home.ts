5f3e405592c9f5197260a2fc78e8d1094d61acbe
mariomo16 <moralesortegamario02@gmail.com>
2026-05-08 11:23:56 +0200

    feat(home): refactor gaming zones section to use dynamic data binding

--- web/src/app/features/home/home.ts
+++ web/src/app/features/home/home.ts
@@ -1,10 +1,32 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { GamingButton } from "../../shared/ui/gaming-button/gaming-button";
 
 @Component({
 	selector: "app-home",
-	imports: [],
+	imports: [GamingButton],
 	templateUrl: "./home.html",
 	styleUrl: "./home.css",
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Home {}
+export class Home {
+	protected readonly zones = [
+		{
+			name: "Standard",
+			description:
+				"Cuenta con 70 ordenadores de alto rendimiento preparados para satisfacer tus necesidades",
+			image: "./images/zones/standard.webp",
+		},
+		{
+			name: "Bootcamp",
+			description:
+				"Ideal tanto para equipos como para los jugadores mas exigentes y en busca de un espacio mas privado",
+			image: "./images/zones/bootcamp.webp",
+		},
+		{
+			name: "Streaming",
+			description:
+				"Si crear contenido es tu objetivo, este es el sitio que buscas, una sala de streaming equipada con todo lo necesario",
+			image: "./images/zones/streaming.webp",
+		},
+	];
+}
