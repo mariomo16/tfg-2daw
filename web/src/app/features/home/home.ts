ab6f40cf8b5e42fc19ecb03f21d4f46f97e5d9dd
mariomo16 <mariomo16.cieza@gmail.com>
2026-05-12 00:18:37 +0200

    feat(home): replace static zones data with dynamic zone resource from ZoneService

--- web/src/app/features/home/home.ts
+++ web/src/app/features/home/home.ts
@@ -4,7 +4,9 @@ import {
 	computed,
 	inject,
 } from "@angular/core";
+import { rxResource } from "@angular/core/rxjs-interop";
 import { AuthService } from "@core/auth/auth.service";
+import { ZoneService } from "@shared/services/zone.service";
 import { LoadingState } from "@shared/ui/states/loading-state/loading-state";
 import { GamingButton } from "../../shared/ui/gaming-button/gaming-button";
 import { Footer } from "../../shared/ui/layout/footer/footer";
@@ -19,29 +21,14 @@ import { Navbar } from "../../shared/ui/layout/navbar/navbar";
 })
 export class Home {
 	readonly #authService = inject(AuthService);
+	readonly #zoneService = inject(ZoneService);
+
 	protected readonly isLoading = computed(() => this.#authService.isLoading());
 	protected readonly isLogged = computed(() =>
 		this.#authService.isAuthenticated(),
 	);
 
-	protected readonly zones = [
-		{
-			name: "Standard",
-			description:
-				"Cuenta con 70 ordenadores de alto rendimiento preparados para satisfacer tus necesidades",
-			image: "./images/zones/standard.webp",
-		},
-		{
-			name: "Bootcamp",
-			description:
-				"Ideal tanto para equipos como para los jugadores mas exigentes y en busca de un espacio mas privado",
-			image: "./images/zones/bootcamp.webp",
-		},
-		{
-			name: "Streaming",
-			description:
-				"Si crear contenido es tu objetivo, este es el sitio que buscas, una sala de streaming equipada con todo lo necesario",
-			image: "./images/zones/streaming.webp",
-		},
-	];
+	protected readonly zoneResource = rxResource({
+		stream: () => this.#zoneService.getAll(),
+	});
 }
