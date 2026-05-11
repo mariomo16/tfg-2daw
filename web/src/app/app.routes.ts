2d79cd7ada21c9ef03eb2a12a95b2d9b6482ef94
mariomo16 <moralesortegamario02@gmail.com>
2026-05-11 17:47:05 +0200

    fix(routes): update import paths for home, register, and login components

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -4,8 +4,22 @@ export const routes: Routes = [
 	{
 		path: "",
 		loadComponent: () =>
-			import("./features/home/home").then((module) => module.Home),
+			import("@features/home/home").then((module) => module.Home),
 		title: "Inicio",
+	},
+	{
+		path: "register",
+		loadComponent: () =>
+			import("@features/auth/register/register").then(
+				(module) => module.Register,
+			),
+		title: "Registrarse",
+	},
+	{
+		path: "login",
+		loadComponent: () =>
+			import("@features/auth/login/login").then((module) => module.Login),
+		title: "Iniciar sesión",
 	},
 	{
 		path: "**",
