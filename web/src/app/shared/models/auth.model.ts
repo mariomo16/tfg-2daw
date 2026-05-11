3c697b7e9c1536f59c625edf47e0643558abc4f7
mariomo16 <moralesortegamario02@gmail.com>
2026-05-11 16:37:06 +0200

    feat(auth): add LoginDto, RegisterDto, and AuthResponse interfaces for authentication

---
+++ web/src/app/shared/models/auth.model.ts
@@ -0,0 +1,18 @@
+import type { UserResponse } from "@shared/models/user.model";
+
+export interface LoginDto {
+	email: string;
+	password: string;
+}
+
+export interface RegisterDto {
+	name: string;
+	email: string;
+	password: string;
+	password_confirmation: string;
+}
+
+export interface AuthResponse {
+	user: UserResponse;
+	token: string;
+}
