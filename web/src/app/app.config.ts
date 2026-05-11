eca2663fc8de0693e378e68fe105a242e4da6525
mariomo16 <moralesortegamario02@gmail.com>
2026-05-11 16:48:14 +0200

    feat(config): enhance app configuration with locale support and auth interceptor

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -1,4 +1,10 @@
-import { provideHttpClient, withFetch } from "@angular/common/http";
+import { registerLocaleData } from "@angular/common";
+import {
+	provideHttpClient,
+	withFetch,
+	withInterceptors,
+} from "@angular/common/http";
+import localeEs from "@angular/common/locales/es";
 import {
 	type ApplicationConfig,
 	LOCALE_ID,
@@ -6,13 +12,16 @@ import {
 	provideZonelessChangeDetection,
 } from "@angular/core";
 import { provideRouter } from "@angular/router";
+import { authInterceptor } from "@core/interceptors/auth.interceptor";
 import { routes } from "./app.routes";
+
+registerLocaleData(localeEs);
 
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideBrowserGlobalErrorListeners(),
 		provideRouter(routes),
-		provideHttpClient(withFetch()),
+		provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
 		provideZonelessChangeDetection(),
 		{ provide: LOCALE_ID, useValue: "es" },
 	],
