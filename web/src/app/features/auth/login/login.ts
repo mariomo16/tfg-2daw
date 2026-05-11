a7ca9051f13b848b83e545f431a88eb56acc8b2b
mariomo16 <mariomo16.cieza@gmail.com>
2026-05-11 21:23:35 +0200

    feat(login): implement login form with validation and submission logic

--- web/src/app/features/auth/login/login.ts
+++ web/src/app/features/auth/login/login.ts
@@ -1,9 +1,62 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import {
+	ChangeDetectionStrategy,
+	Component,
+	inject,
+	signal,
+} from "@angular/core";
+import {
+	email,
+	FormField,
+	FormRoot,
+	form,
+	required,
+} from "@angular/forms/signals";
+import { Router, RouterLink } from "@angular/router";
+import { AuthService } from "@core/auth/auth.service";
+import type { LoginDto } from "@shared/models/auth.model";
+import { GamingButton } from "@shared/ui/gaming-button/gaming-button";
 
 @Component({
 	selector: "app-login",
-	imports: [],
+	imports: [FormRoot, FormField, RouterLink, GamingButton],
 	templateUrl: "./login.html",
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Login {}
+export class Login {
+	readonly #authService = inject(AuthService);
+	readonly #router = inject(Router);
+
+	protected readonly isLoading = this.#authService.isLoading;
+
+	// https://dev.to/abp_io/signal-based-forms-in-angular-21-why-youll-never-miss-reactive-forms-again-n7l
+	// https://angular.dev/guide/forms/signals/validation
+	protected readonly registerModel = signal<LoginDto>({
+		email: "",
+		password: "",
+	});
+
+	protected readonly form = form(this.registerModel, (schemaPath) => {
+		required(schemaPath.email, { message: "El correo es obligatorio" });
+		email(schemaPath.email, {
+			message: "Introduce una dirección de correo válida",
+		});
+
+		required(schemaPath.password, {
+			message: "La contraseña no puede estar vacía",
+		});
+	});
+
+	onSubmit(): void {
+		if (!this.form().valid()) {
+			this.form().markAsTouched();
+			return;
+		}
+
+		const data = this.form().value();
+
+		this.#authService.login(data).subscribe({
+			next: () => this.#router.navigate(["/"]),
+			error: (err) => console.error("Error al iniciar sesión:", err),
+		});
+	}
+}
