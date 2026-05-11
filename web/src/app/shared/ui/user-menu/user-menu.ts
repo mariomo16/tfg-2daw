5507c57fd10d4a46d99f46130bd56eda6c4fb898
mariomo16 <mariomo16.cieza@gmail.com>
2026-05-11 23:55:24 +0200

    feat(user-menu): implement user menu component with profile link and logout functionality

--- web/src/app/shared/ui/user-menu/user-menu.ts
+++ web/src/app/shared/ui/user-menu/user-menu.ts
@@ -1,10 +1,22 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
+import { Router, RouterLink } from "@angular/router";
+import { AuthService } from "@core/auth/auth.service";
 
 @Component({
 	selector: "app-user-menu",
-	imports: [],
+	imports: [RouterLink],
 	templateUrl: "./user-menu.html",
-	styleUrl: "./user-menu.css",
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UserMenu {}
+export class UserMenu {
+	readonly #router = inject(Router);
+	readonly #authService = inject(AuthService);
+	protected readonly loggedUser = this.#authService.user;
+
+	protected onLogout() {
+		this.#authService.logout().subscribe({
+			next: () => this.#router.navigate(["/"]),
+			error: (err) => console.error("No se ha podido cerrar sesion:", err),
+		});
+	}
+}
