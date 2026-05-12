3741cc6e2e98841b43163630870fca0c13798a83
mariomo16 <mariomo16.cieza@gmail.com>
2026-05-12 19:47:12 +0200

    feat(normal-button): simplify button component by removing buttonStyle and adding CSS styles

--- web/src/app/shared/ui/normal-button/normal-button.ts
+++ web/src/app/shared/ui/normal-button/normal-button.ts
@@ -5,13 +5,11 @@ import { RouterLink } from "@angular/router";
 	selector: "app-normal-button",
 	imports: [RouterLink],
 	templateUrl: "./normal-button.html",
+	styleUrl: "./normal-button.css",
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NormalButton {
 	readonly useRouterLink = input<boolean>(true);
 	readonly label = input.required<string>();
 	readonly href = input<string>();
-
-	protected readonly buttonStyle =
-		"rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-light transition-colors duration-150";
 }
