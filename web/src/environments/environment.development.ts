b10ba59e143e148091916d566fe48290569253bd
mariomo16 <mariomo16.cieza@gmail.com>
2026-05-12 21:34:53 +0200

    fix(environment): update API and storage URLs for development and production environments

--- web/src/environments/environment.development.ts
+++ web/src/environments/environment.development.ts
@@ -1,5 +1,11 @@
+// export const environment = {
+// 	production: false,
+// 	apiUrl: "http://localhost:8000/api/v1",
+// 	storageUrl: "http://localhost:8000/storage",
+// } as const;
+
 export const environment = {
 	production: false,
-	apiUrl: "http://localhost:8000/api/v1",
-	storageUrl: "http://localhost:8000/storage",
+	apiUrl: "https://tfg-2daw-develop.onrender.com/api/v1",
+	storageUrl: "https://tfg-2daw-develop.onrender.com/storage",
 } as const;
--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
 	production: true,
-	apiUrl: "",
-	storageUrl: "",
+	apiUrl: "https://tfg-2daw-main.onrender.com/api/v1",
+	storageUrl: "https://tfg-2daw-main.onrender.com/storage",
 } as const;
