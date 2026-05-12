0efb6c27279211cbd9be1500a296ce27f0238c76
mariomo16 <mariomo16.cieza@gmail.com>
2026-05-12 22:23:12 +0200

    fix(environment): update API and storage URLs to match development environment

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
 	production: true,
-	apiUrl: "https://tfg-2daw-main.onrender.com/api/v1",
-	storageUrl: "https://tfg-2daw-main.onrender.com/storage",
+	apiUrl: "https://tfg-2daw-develop.onrender.com/api/v1",
+	storageUrl: "https://tfg-2daw-develop.onrender.com/storage",
 } as const;
