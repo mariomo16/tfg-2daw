e446aa3e209d7b79d0d2bfb7d83d900c73819df2
mariomo16 <mariomo16.cieza@gmail.com>
2026-05-12 21:59:07 +0200

    fix(environment): revert API and storage URLs to local development settings

--- web/src/environments/environment.development.ts
+++ web/src/environments/environment.development.ts
@@ -1,11 +1,5 @@
-// export const environment = {
-// 	production: false,
-// 	apiUrl: "http://localhost:8000/api/v1",
-// 	storageUrl: "http://localhost:8000/storage",
-// } as const;
-
 export const environment = {
 	production: false,
-	apiUrl: "https://tfg-2daw-develop.onrender.com/api/v1",
-	storageUrl: "https://tfg-2daw-develop.onrender.com/storage",
+	apiUrl: "http://localhost:8000/api/v1",
+	storageUrl: "http://localhost:8000/storage",
 } as const;
